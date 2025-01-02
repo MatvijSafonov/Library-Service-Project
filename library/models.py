7e2248a8a611b56b6270bec823b9320d7db68476
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-01-02 15:43:54 +0200

    Fix typos in daily_fee attribute and in users app test.py

--- library/models.py
+++ library/models.py
@@ -22,4 +22,4 @@ class Book(models.Model):
         ]
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.dayly_fee})"
+        return f"{self.title} by {self.author} ({self.daily_fee})"
