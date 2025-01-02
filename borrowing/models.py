439f1dcfe097c017202259b4dd203078645016b7
Matvii Safonov <matveysafonov2@gmail.com>
2025-01-02 17:58:22 +0200

    Add borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from library.models import Book
+from user.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_date = models.DateField()
+    actual_date = models.DateField(blank=True, null=True)
+    book_id = models.ManyToManyField(Book, related_name="borrowings")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"ID-{self.id} ({self.borrow_date}-{self.expected_date})"
+
