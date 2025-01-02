867af5abc490f2ec19360926bbaf1e4114f46bf5
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-01-02 20:48:13 +0200

    Add ordering by "-created_at" for Payment model

--- payment/models.py
+++ payment/models.py
@@ -36,5 +36,8 @@ class Payment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_at"]
+
     def __str__(self) -> str:
         return f"{self.type} payment for {self.borrowing} - {self.status}"
