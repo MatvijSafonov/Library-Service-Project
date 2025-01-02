30a225dc95742dd6385eafb600be3dcd545204b8
Rabb1t-Artem <79219648+Rabb1t-Artem@users.noreply.github.com>
2025-01-02 14:43:12 +0200

    add book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = (
+        ("soft", "Soft"),
+        ("hard", "Hard"),
+    )
+
+    title = models.CharField(max_length=63)
+    author = models.CharField(max_length=63)
+    cover = models.CharField(choices=COVER_CHOICES, max_length=7)
+    inventory = models.PositiveIntegerField()
+    dayly_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        ordering = ("title",)
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "author"], name="unique_title_author"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.dayly_fee})"
