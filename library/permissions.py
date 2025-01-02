3d25484a7d533a5956c8975273e496cb42d4aa9f
FursykIvan <ivan.fursyk@gmail.com>
2025-01-02 15:37:42 +0200

    Add permissions to the Books Service

--- library/permissions.py
+++ library/permissions.py
@@ -1,26 +1,20 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.request import Request
 
 
-class CustomPermission(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     """
     Only admin users can create/update/delete books
     All users (even unauthenticated ones) should be able to list books
     """
-    def has_permission(self, request, view):
-        if request.method in ("GET", "HEAD", "OPTIONS"):
-            return True
-        elif request.method in ("POST", "PUT", "PATCH", "DELETE"):
-            return request.user.is_authenticated and request.user.is_staff
-        return False
-
-
-# class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
-#     def has_permission(self, request, view):
-#         return bool(
-#             (
-#                 request.method in SAFE_METHODS
-#                 and request.user
-#                 and request.user.is_authenticated
-#             )
-#             or (request.user and request.user.is_staff)
-#         )
+    def has_permission(
+            self,
+            request: Request,
+            view: object) -> bool:
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+            )
+            or (request.user and request.user.is_staff)
+        )
