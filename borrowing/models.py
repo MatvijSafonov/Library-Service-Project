d075a5e33829306653cb2f4f38ec2c1909893c3a
Matvii Safonov <matveysafonov2@gmail.com>
2025-01-03 10:43:07 +0200

    rewrite borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 from library.models import Book
-from user.models import User
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
-    expected_date = models.DateField()
-    actual_date = models.DateField(blank=True, null=True)
-    book_id = models.ManyToManyField(Book, related_name="borrowings")
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"ID-{self.id} ({self.borrow_date}-{self.expected_date})"
-
+        return f"ID - {self.id} ({self.borrow_date} - {self.expected_return_date})"
