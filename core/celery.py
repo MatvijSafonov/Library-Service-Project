311aebf4b56844011de4ae6f05d03f12c449b0c9
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-01-05 15:17:19 +0200

    transferred task check_overdue_borrowings to app borrowing

--- core/celery.py
+++ core/celery.py
@@ -12,8 +12,8 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     "check-overdue-borrowings-every-day": {
-        "task": "payment.tasks.check_overdue_borrowings",
-        "schedule": crontab(hour=7, minute=0),
+        "task": "borrowing.tasks.check_overdue_borrowings",
+        "schedule": crontab("*"),
     },
     "check-payment-sessions": {
         "task": "payment.tasks.check_payment_sessions",
