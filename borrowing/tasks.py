042d578181a92d73a95c2f586fc3ca95ca9ad7b7
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-01-05 15:26:26 +0200

    Update tasks.py

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -15,6 +15,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 
 def send_telegram_message(message):
+     """
+    Sends a message to a predefined Telegram chat.
+    """
     try:
         response = requests.post(
             TELEGRAM_API_URL,
@@ -27,6 +30,11 @@ def send_telegram_message(message):
 
 @shared_task
 def check_overdue_borrowings():
+    """
+    Checks for borrowings that are overdue and sends a notification 
+    to a Telegram chat for each overdue borrowing. If no overdue 
+    borrowings are found, sends a notification indicating this.
+    """
     today = now().date()
     overdue_borrowings = Borrowing.objects.filter(
         expected_return_date__lte=today, actual_return_date__isnull=True
