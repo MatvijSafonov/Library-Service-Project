bc46a1505ec43b2e480e7d24483f1cad95328b8c
Matvii Safonov <matveysafonov2@gmail.com>
2025-01-06 10:57:09 +0200

    merge commit

--- borrowing/views.py
+++ borrowing/views.py
@@ -2,14 +2,11 @@ import datetime
 from typing import Type
 
 import stripe
-from django.db import transaction
-from django.shortcuts import get_object_or_404
-from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.request import Request
 from rest_framework.serializers import Serializer
-from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
