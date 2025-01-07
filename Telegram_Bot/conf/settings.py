01ceb8406321ee56a01fd5bfc9fe5d8eeee1ec53
Rabb1t-Artem <79219648+Rabb1t-Artem@users.noreply.github.com>
2025-01-07 14:23:33 +0200

    add telegram bot

---
+++ Telegram_Bot/conf/settings.py
@@ -0,0 +1,35 @@
+import os
+import logging
+
+from aiogram import Bot
+import betterlogging as bl
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+
+
+def setup_logging():
+    """
+    Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+    """
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
