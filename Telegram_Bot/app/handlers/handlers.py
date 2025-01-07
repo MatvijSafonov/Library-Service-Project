ced9b472c9c5e799ac980e2837fbb350c3f63b45
Rabb1t-Artem <79219648+Rabb1t-Artem@users.noreply.github.com>
2025-01-07 17:17:08 +0200

    fix handlers

--- Telegram_Bot/app/handlers/handlers.py
+++ Telegram_Bot/app/handlers/handlers.py
@@ -238,11 +238,13 @@ async def show_borrowings(callback: CallbackQuery, state: FSMContext):
         formatted_text = "📚 Your Borrowed Books:\n\n"
 
         for item in borrowings["results"]:
+            status_emoji = "✅" if item["actual_return_date"] else "❌"
             formatted_text += (
                 f"📖 Borrowing ID: {item['id']}\n"
                 f"📅 Borrow Date: {item['borrow_date']}\n"
                 f"⏳ Return Date: {item['expected_return_date']}\n"
-                f"✅ Returned: {item['actual_return_date'] or 'Not returned'}\n\n"
+                f"{status_emoji} Returned: "
+                f"{item['actual_return_date'] or 'Not returned'}\n\n"
             )
 
         keyboard = get_borrowings_pagination_keyboard(
